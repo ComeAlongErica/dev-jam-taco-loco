9ce8b179fe7f3eb778bb2edc8466a5b41431f95b
ComeAlongErica <ericaraephoto@gmail.com>
2020-07-29 22:58:52 -0400

    Updates

---
+++ precache-manifest.6268f04832593f201617ada47040dce1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341064a79e9579b5f85c3168d3fc0961",
+    "url": "/dev-jam-taco-loco/index.html"
+  },
+  {
+    "revision": "ded8e1ffe53d0c9bb47c",
+    "url": "/dev-jam-taco-loco/static/js/2.73a3b5ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dev-jam-taco-loco/static/js/2.73a3b5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8074be5064752f4dec",
+    "url": "/dev-jam-taco-loco/static/js/main.8899290c.chunk.js"
+  },
+  {
+    "revision": "7c3d4827351f5afea60d",
+    "url": "/dev-jam-taco-loco/static/js/runtime-main.c503f96b.js"
+  }
+]);
